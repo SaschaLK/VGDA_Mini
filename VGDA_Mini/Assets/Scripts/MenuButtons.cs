9f25a4860166bf8f095d38db9752e11754f4b9fb
baterra <32109799+baterra@users.noreply.github.com>
2018-10-05 19:34:25 -0700

    Main menu that loads a new scene with a gameobject that doesn't get destroyed when loading this new scene

---
+++ VGDA_Mini/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuButtons : MonoBehaviour {
+
+   
+
+    public void LoadScene(string SceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void ExitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
