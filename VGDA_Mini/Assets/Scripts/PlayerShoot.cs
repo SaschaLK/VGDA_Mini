976bd5ec8fb00774bfe11598ed8004df997ff107
Kenny Nguyen <baterra602@gmail.com>
2018-11-27 09:52:06 -0800

    attempts at fixing raycsat

--- VGDA_Mini/Assets/Scripts/PlayerShoot.cs
+++ VGDA_Mini/Assets/Scripts/PlayerShoot.cs
@@ -15,37 +15,29 @@ public class PlayerShoot : MonoBehaviour {
 	void Update () {
         if (Input.GetButtonDown("Fire1"))
         {
-            //Debug.Log("Did");
-            RaycastHit hit;
-            
-            Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-
-            Vector2 raycastDir = new Vector2(mousePos.x - playerTransform.position.x, mousePos.y - playerTransform.position.y);
-
-         
-            //Debug.Log("X");
-            // Debug.Log(raycastDir.x);
-            //Debug.Log("Y");
-            // Debug.Log(raycastDir.y);
-
-            Debug.DrawRay(playerTransform.position,raycastDir, Color.red, 5);
-            if (Physics.Raycast(playerTransform.position, raycastDir, out hit, shootRange))
-            {
-                hit.collider.gameObject.SetActive(false);
-                Debug.Log("Did  Hit");
-            }
-            //shoot();
+            shoot();
         }
-
-
     }
 
     public void shoot() {
-       
+
         RaycastHit hit;
-        if (Physics.Raycast(playerTransform.position, Input.mousePosition, out hit, shootRange))
+
+        Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+
+        Vector2 raycastDir = new Vector2(mousePos.x - playerTransform.position.x, mousePos.y - playerTransform.position.y);
+
+
+        //Debug.Log("X");
+        // Debug.Log(raycastDir.x);
+  
+
+        Debug.DrawRay(playerTransform.position, raycastDir, Color.red, 5);
+
+        if (Physics.Raycast(playerTransform.position, raycastDir, out hit, shootRange))
         {
             hit.collider.gameObject.SetActive(false);
+            Debug.Log("Did  Hit");
         }
     }
 
