f8afb4d3b3675d286e0af68d88c5af7c4ec17eec
Kenny Nguyen <baterra602@gmail.com>
2018-11-27 09:26:42 -0800

    Fixed the part of the raycasting where it creates a ray in the correct direction from mouse click. Currently working on fixing the collision detecting part of the raycast.

--- VGDA_Mini/Assets/Scripts/PlayerShoot.cs
+++ VGDA_Mini/Assets/Scripts/PlayerShoot.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
     public Transform playerTransform;
     public float shootRange = 100f;
-	// Use this for initialization
+    // Use this for initialization
+    Camera cam;
 	void Start () {
-		
+        cam = Camera.main;
 	}
 	
 	// Update is called once per frame
@@ -16,14 +17,16 @@ public class PlayerShoot : MonoBehaviour {
         {
             //Debug.Log("Did");
             RaycastHit hit;
-            Vector2 mousePos = Input.mousePosition;
-            Vector2 raycastDir = //Input.mousePosition - playerTransform.position  ;
-            new Vector2(mousePos.x - playerTransform.position.x, mousePos.y - playerTransform.position.y);
+            
+            Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
 
-            Debug.Log("X");
-            Debug.Log(raycastDir.x);
-            Debug.Log("Y");
-            Debug.Log(raycastDir.y);
+            Vector2 raycastDir = new Vector2(mousePos.x - playerTransform.position.x, mousePos.y - playerTransform.position.y);
+
+         
+            //Debug.Log("X");
+            // Debug.Log(raycastDir.x);
+            //Debug.Log("Y");
+            // Debug.Log(raycastDir.y);
 
             Debug.DrawRay(playerTransform.position,raycastDir, Color.red, 5);
             if (Physics.Raycast(playerTransform.position, raycastDir, out hit, shootRange))
