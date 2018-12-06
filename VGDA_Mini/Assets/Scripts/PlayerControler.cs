27513c284e83089247f70226816d0b1251c7b8bd
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-12-05 21:35:21 -0800

    player animation, including left and right

--- VGDA_Mini/Assets/Scripts/PlayerControler.cs
+++ VGDA_Mini/Assets/Scripts/PlayerControler.cs
@@ -8,37 +8,44 @@ public class PlayerControler : MonoBehaviour {
     public float jumpForce = 4;
     public float movementSpeed = 5;
     private bool onFloor = true; //to see if it's grounded
+    private float horizontalInput;
+    private Animator playerAnimator;
+    private Vector3 right = new Vector3(-1,1,1);
+    private Vector3 left = new Vector3(1,1,1);
 
-    // Use this for initialization
-    void Start()
-    {
-
+    private void Start() {
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        movePlayer();
+    private void Update() {
+        MovePlayer();
     }
 
     // this function makes the player move left, right and jump.
-    void movePlayer()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
+    void MovePlayer() {
+        if(Input.GetAxis("Horizontal") > 0.5) {
+            transform.localScale = right;
+            playerAnimator.SetTrigger("playerWalk");
+        }
+        else if(Input.GetAxis("Horizontal") < -0.5) {
+            transform.localScale = left;
+            playerAnimator.SetTrigger("playerWalk");
+        }
+        else {
+            playerAnimator.SetTrigger("playerIdle");
+        }
 
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, 0);
 
-        if (Input.GetButtonDown("Jump") && onFloor)
-        {
+        if (Input.GetButtonDown("Jump") && onFloor) {
             myRigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             onFloor = false;
         }
     }
     //check the collision with the floor to avoid jumping in the air
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Floor") && onFloor == false)
-        {
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Floor") && onFloor == false) {
             onFloor = true;
         }
     }
