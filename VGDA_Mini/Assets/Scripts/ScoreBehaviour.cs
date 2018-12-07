7b8c5970e5daa94169d6b412e74772440abeb4b1
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-12-06 16:03:22 -0800

    added time score

---
+++ VGDA_Mini/Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehaviour : MonoBehaviour {
+
+    //private float score;
+    private Text score;
+
+    private void Awake() {
+        score = GetComponent<Text>();
+    }
+
+    private void Update() {
+        score.text = "Score: " + Time.timeSinceLevelLoad.ToString();
+    }
+
+}
